dd3b940c8d715a709f78d8eaf0f0dcc1b17e1c6a
MihirSaiDudekula <mihirsaidudekula@gmail.com>
2024-11-20 00:11:47 +0530

    Update page.js

--- src/app/registration/page.js
+++ src/app/registration/page.js
@@ -82,7 +82,7 @@ export default function Home() {
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative py-5">
                   <input onChange={handleChange} value={student.name} autoComplete="off" id="name" name="name" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Name " />
-                  <label htmlFor="name" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Name</label>
+                  <label htmlFor="name" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Name of the boy</label>
                 </div>
                 <div className="relative py-5">
                   <input onChange={handleChange} value={student.usn} autoComplete="off" id="usn" name="usn" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="USN" />
